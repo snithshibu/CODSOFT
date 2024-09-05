16217b3a5a9df7073b0d85fca43bf2da1e7fd92d
Snith SHibu <snithshibu.b23ec1156@mbcet.ac.in>
2024-09-05 20:44:28 +0530

    Commit Changes

--- Task 2/calculator.py
+++ Task 2/calculator.py
@@ -3,18 +3,21 @@ Problem statement: Design a simple calculator with basic arithmetic operations.
 Prompt the user to input two numbers and an operation choice. 
 Perform the calculation and display the result.
 """
+
 #functions for different operations
 def add(x,y):
-    return x+y
+    return x+y #addition
 def subtract(x,y):
-    return x-y
+    return x-y #subtraction
 def multiply(x,y):
-    return x*y
-def divide(x,y):
-    return x/y
+    return x*y #multiplication
+def quotient(x,y):
+    return x/y #division with quotient
+def remainder(x,y):
+    return x%y #remainder of division
 
 #welcoming statement
-print(f"Welcome to calculator. \nRules are simple: select two numbers, choose an operator and note down the value.")
+print(f"Welcome to calculator. \nRules are simple: select two numbers, choose an operator and find the result.")
 print(f"Enter 1 for addition. \nEnter 2 for subtraction. \nEnter 3 for multiplication. \nEnter 4 for division.")
 
 #for n number of trials
@@ -25,16 +28,17 @@ while True:
 
     if choice == 1:
         sum = add(num_1,num_2)
-        print(f"Sum of {num_1} and {num_2} is {sum}")
+        print(f"Sum of {num_1} and {num_2} is {sum}.")
     elif choice == 2:
         diff = subtract(num_1,num_2)
-        print(f"Difference of {num_1} and {num_2} is {diff}")
+        print(f"Difference of {num_1} and {num_2} is {diff}.")
     elif choice == 3:
         prod = multiply(num_1,num_2)
-        print(f"Sum of {num_1} and {num_2} is {prod}")
+        print(f"Product of {num_1} and {num_2} is {prod}.")
     elif choice == 4:
-        quo = divide(num_1,num_2)
-        print(f"Sum of {num_1} and {num_2} is {quo}")
+        quo = quotient(num_1,num_2)
+        rem = remainder(num_1,num_2)
+        print(f"Quotient of {num_1} and {num_2} is {quo}. Remainder is {rem}")
     else:
         print("Enter valid operator choice. Try again.")
 
